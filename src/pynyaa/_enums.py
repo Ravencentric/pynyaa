701e7d5030dc5953caae0af0a76146cb54cbe6c4
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2025-08-30 21:58:36 +0530

    fix: strenum in <3.11

--- src/pynyaa/_enums.py
+++ src/pynyaa/_enums.py
@@ -11,7 +11,8 @@ else:
     from enum import Enum
 
     class StrEnum(str, Enum):
-        pass
+        def __str__(self) -> str:
+            return self.value
 
     class IntEnum(enum.IntEnum):
         def __str__(self) -> str:
